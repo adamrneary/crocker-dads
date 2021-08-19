0162e02b3809348a3d66ed8022ca349b9baca049
Adam Neary <adamrneary@users.noreply.github.com>
2021-08-19 16:41:54 -0700

    Fix the Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,15 @@
+import { Fragment } from 'react';
+
+import { Popover, Transition } from '@headlessui/react';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
+const navigation = [
+  { name: 'About', href: '#about' },
+  { name: 'Leadership', href: '#leadership' },
+  { name: 'Clubhouse', href: '#clubhouse' },
+  { name: 'Events', href: '#events' },
+];
+
 const HERO_SRC =
   'https://images.unsplash.com/photo-1597078889966-e375fdcd95b4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80';
 
@@ -16,7 +28,7 @@ export default function Hero() {
             <polygon points="50,0 100,0 50,100 0,100" />
           </svg>
 
-          <div>
+          <Popover>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
               <nav
                 className="relative flex items-center justify-between sm:h-10 lg:justify-start"
@@ -31,47 +43,23 @@ export default function Hero() {
                     />
 
                     <div className="-mr-2 flex items-center md:hidden">
-                      <button
-                        type="button"
-                        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-600"
-                        aria-expanded="false"
-                      >
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-600">
                         <span className="sr-only">Open main menu</span>
-                        <svg
-                          className="h-6 w-6"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          aria-hidden="true"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h16M4 18h16"
-                          />
-                        </svg>
-                      </button>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
                     </div>
                   </div>
                 </div>
                 <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8 text-gray-600">
-                  <a href="#about" className="hover:text-gray-900">
-                    About
-                  </a>
-
-                  <a href="#leadership" className="hover:text-gray-900">
-                    Leadership
-                  </a>
-
-                  <a href="#clubhouse" className="hover:text-gray-900">
-                    Clubhouse
-                  </a>
-                  <a href="#events" className="hover:text-gray-900">
-                    Events
-                  </a>
-
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="hover:text-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
                   <a
                     href="https://groups.google.com/g/crocker-dads-club"
                     className="text-blue-600 font-bold hover:text-blue-500"
@@ -81,91 +69,65 @@ export default function Hero() {
                 </div>
               </nav>
             </div>
-            <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-              <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div className="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark-blue.svg"
-                      alt=""
-                    />
+
+            <Transition
+              as={Fragment}
+              enter="duration-150 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+              >
+                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="px-5 pt-4 flex items-center justify-between">
+                    <div>
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/workflow-mark-blue.svg"
+                        alt=""
+                      />
+                    </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Close main menu</span>
+                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
                   </div>
-                  <div className="-mr-2">
-                    <button
-                      type="button"
-                      className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-600 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
-                    >
-                      <span className="sr-only">Close main menu</span>
-                      <svg
-                        className="h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
+                  <div className="px-2 pt-2 pb-3 space-y-1 font-bold text-gray-700 hover:text-gray-900">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md hover:bg-gray-50"
                       >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M6 18L18 6M6 6l12 12"
-                        />
-                      </svg>
-                    </button>
+                        {item.name}
+                      </a>
+                    ))}
                   </div>
-                </div>
-                <div className="px-2 pt-2 pb-3 space-y-1 font-bold text-gray-700 hover:text-gray-900">
                   <a
-                    href="#about"
-                    className="block px-3 py-2 rounded-md hover:bg-gray-50"
+                    href="https://groups.google.com/g/crocker-dads-club"
+                    className="block w-full px-5 py-3 text-center text-blue-600 bg-gray-50 hover:bg-gray-100 font-bold"
                   >
-                    About
-                  </a>
-
-                  <a
-                    href="#leadership"
-                    className="block px-3 py-2 rounded-md  hover:bg-gray-50"
-                  >
-                    Leadership
-                  </a>
-
-                  <a
-                    href="#clubhouse"
-                    className="block px-3 py-2 rounded-md hover:text-gray-900 hover:bg-gray-50"
-                  >
-                    Clubhouse
-                  </a>
-
-                  <a
-                    href="#events"
-                    className="block px-3 py-2 rounded-md  hover:text-gray-900 hover:bg-gray-50"
-                  >
-                    Events
+                    Join Us
                   </a>
                 </div>
-                <a
-                  href="https://groups.google.com/g/crocker-dads-club"
-                  className="block w-full px-5 py-3 text-center text-blue-600 bg-gray-50 hover:bg-gray-100 font-bold"
-                >
-                  Join Us
-                </a>
-              </div>
-            </div>
-          </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
 
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl leading-tight tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">
-                  Don’t just be a dad.
-                  <br />
-                  Be a&nbsp;
-                </span>
-                <span className="block text-blue-600 xl:inline">
-                  Crocker Dad
-                </span>
-                .
+                Don’t just be a dad.
+                <br />
+                Be a&nbsp;
+                <span className="text-blue-600">Crocker Dad</span>.
               </h1>
               <p className="mt-3 text-base font-serif text-gray-600 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 We are an elite force of fund-raising, grilling, grilling,
